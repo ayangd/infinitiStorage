ce4f20df2c105a868c86f0383f5d0dd06ce5ee81
ayangd <jimmyhendrixx90@gmail.com>
2021-10-29 23:49:52 +0700

    Add background

--- frontend/src/layout/login/container-components.tsx
+++ frontend/src/layout/login/container-components.tsx
@@ -3,6 +3,7 @@ import { chakra } from '@chakra-ui/system';
 
 export const Container = chakra(Flex, {
     baseStyle: {
+        backgroundColor: 'teal',
         alignItems: 'center',
         justifyContent: 'center',
         w: '100vw',
@@ -12,8 +13,10 @@ export const Container = chakra(Flex, {
 
 export const CenteredContainer = chakra(Flex, {
     baseStyle: {
+        backgroundColor: 'white',
         border: '1px solid lightgray',
         borderRadius: '8px',
+        boxShadow: '2px 2px 2px gray',
         p: '32px',
         flexDirection: 'column',
         gap: '16px',
