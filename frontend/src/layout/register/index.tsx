7be14c64d03da07fc258c60ca74be42d6f5acb67
ayangd <jimmyhendrixx90@gmail.com>
2021-11-12 23:17:56 +0700

    Delete unused scss

--- frontend/src/layout/register/index.tsx
+++ frontend/src/layout/register/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import * as auth from '../../lib/auth';
 import { useHistory } from 'react-router';
-import './style.scss';
 import {
     CenteredContainer,
     Container,
