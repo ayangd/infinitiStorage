4359ac762127ca7479cadb288874186cee634ffe
ayangd <jimmyhendrixx90@gmail.com>
2021-11-12 23:42:59 +0700

    Add explicit type on input type

--- frontend/src/formik/helpers.tsx
+++ frontend/src/formik/helpers.tsx
@@ -6,10 +6,34 @@ import {
 import { Input } from '@chakra-ui/input';
 import { FieldProps } from 'formik';
 
+type InputType =
+    | 'button'
+    | 'checkbox'
+    | 'color'
+    | 'date'
+    | 'datetime-local'
+    | 'email'
+    | 'file'
+    | 'hidden'
+    | 'image'
+    | 'month'
+    | 'number'
+    | 'password'
+    | 'radio'
+    | 'range'
+    | 'reset'
+    | 'search'
+    | 'submit'
+    | 'tel'
+    | 'text'
+    | 'time'
+    | 'url'
+    | 'week';
+
 export function createField<T>(
     fieldName: keyof T,
     label: string,
-    type: string
+    type: InputType
 ) {
     type createFieldProps = FieldProps<string, T>;
     return ({ field, form }: createFieldProps) => (
